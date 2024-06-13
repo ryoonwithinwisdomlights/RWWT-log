5fe498aaf17f7bbcf426b3a7dbaa3df9737caaf6
ryoon-with-wisdomtrees <ryoon.with.wisdomtrees@gmail.com>
2024-06-14 00:14:37 +0900

    메인연결 페이지 변경

--- themes/gitbook/config.js
+++ themes/gitbook/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  INDEX_PAGE: 'about', // Articles displayed on the document homepage, please make sure this path is included in your notice database
+  INDEX_PAGE: 'main', // Articles displayed on the document homepage, please make sure this path is included in your notice database
 
   AUTO_SORT: process.env.NEXT_PUBLIC_GITBOOK_AUTO_SORT || true,
   // 카테고리 이름별로 기사를 자동으로 정렬할지 여부, 자동 그룹화는 Notion의 기사 순서를 방해할 수 있습니다.
